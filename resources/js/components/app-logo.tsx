3aeeac1da6074e25cf94e9b1eed3f92b84b57667
Juan Alexanov <ajajuan10@gmail.com>
2025-05-30 02:51:59 +0700

    Update app-logo.tsx

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,10 +1,12 @@
-import AppLogoIcon from './app-logo-icon';
+import { Presentation } from 'lucide-react';
+// import AppLogoIcon from './app-logo-icon';
 
 export default function AppLogo() {
     return (
         <>
             <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+                {/* <AppLogoIcon className="size-5 fill-current text-white dark:text-black" /> */}
+                <Presentation className='size-5 text-white dark:text-black' />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-none font-semibold">Seminar App 2025</span>
